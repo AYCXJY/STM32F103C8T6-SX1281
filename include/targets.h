722bfc09802a3e6dc73da6d166fc7a1a87264b13
xjy <2444717591@qq.com>
2024-09-22 10:25:58 +0800

    添加TX_EN和RX_EN

--- include/targets.h
+++ include/targets.h
@@ -13,6 +13,10 @@
 #define GPIO_PIN_RST         PB4
 #define GPIO_PIN_BUSY        PC15
 
+#define GPIO_PIN_TX_EN       PB7
+#define GPIO_PIN_RX_EN       PB6
+
+
 // #define GPIO_PIN_RCSIGNAL_RX PB7  // USART1, AFAIO
 // #define GPIO_PIN_RCSIGNAL_TX PB6  // USART1, AFAIO
 
@@ -20,7 +24,7 @@
 
 // Output Power - use default SX1280
 
-#define POWER_OUTPUT_FIXED 13 //MAX power for 2400 RXes that doesn't have PA is 12.5dbm
+// #define POWER_OUTPUT_FIXED 13 //MAX power for 2400 RXes that doesn't have PA is 12.5dbm
 /*************************************** */
 
 
