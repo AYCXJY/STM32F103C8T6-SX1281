c3dcbbf34eebc9479a13701a70240a5582ba3e6b
xjy <2444717591@qq.com>
2024-09-22 12:59:02 +0800

    添加FHSS，优化调试，删除多余代码

--- src/rx_main.cpp
+++ src/rx_main.cpp
@@ -4,83 +4,68 @@
 #include "common.h"
 #include "FHSS.h"
 
-// uint8_t FHSShopInterval = 4;    
+uint8_t FHSShopInterval = 4;    
+uint8_t IntervalCount = 0;
 
-// uint8_t  IntervalCount = 0;uint8_t  i = 0;
-
-void ICACHE_RAM_ATTR TXdoneCallback()
-{
-  Serial.print("txcallback");
-}
+void ICACHE_RAM_ATTR TXdoneCallback(){}
 
 bool ICACHE_RAM_ATTR RXdoneCallback(SX12xxDriverCommon::rx_status const /*status*/)
 {
+  // 如果接收到数据，LED 灯会和发送方同时闪烁。
   digitalToggle(PC13);
+  // 打印 RXdataBuffer
   for (int i = 0; i < 16; i++)
   {
       Serial.print((char)Radio.RXdataBuffer[i]);
   }
   Serial.println("");
+
+  // 打印状态信息
+  Serial.println("RSSI = " + Radio.GetRssiInst(SX12XX_Radio_1));
+  Radio.GetStatus(SX12XX_Radio_1);
+
   return true;
 }
 
-void SetRFLinkRate(uint8_t index) 
+// 初始化空中速率
+void SetRFLinkRate(uint8_t index)
 {
   expresslrs_mod_settings_s *const ModParams = get_elrs_airRateConfig(index);
-  // expresslrs_rf_pref_params_s *const RFperf = get_elrs_RFperfParams(index);
-  // Binding always uses invertIQ
-  bool invertIQ = InBindingMode || (UID[5] & 0x01);
-
-  // FHSSusePrimaryFreqBand = !(ModParams->radio_type == RADIO_TYPE_LR1121_LORA_2G4) && !(ModParams->radio_type == RADIO_TYPE_LR1121_GFSK_2G4);
-  // FHSSuseDualBand = ModParams->radio_type == RADIO_TYPE_LR1121_LORA_DUAL;
 
   Radio.Config(ModParams->bw, ModParams->sf, ModParams->cr, FHSSgetInitialFreq(), 
-              ModParams->PreambleLen, invertIQ, ModParams->PayloadLength, ModParams->interval, 
+              ModParams->PreambleLen, false, ModParams->PayloadLength, ModParams->interval, 
               uidMacSeedGet(), 0, (ModParams->radio_type == RADIO_TYPE_SX128x_FLRC));
 
-
-  // Radio.FuzzySNRThreshold = (RFperf->DynpowerSnrThreshUp == DYNPOWER_SNR_THRESH_NONE) ? 0 : (RFperf->DynpowerSnrThreshUp - RFperf->DynpowerSnrThreshDn);
-
-  // InitialFreq has been set, so lets also reset the FHSS Idx and Nonce.
-  // FHSSsetCurrIndex(0);
-
   ExpressLRS_currAirRate_Modparams = ModParams;
-  // ExpressLRS_currAirRate_RFperfParams = RFperf;
-
 }
 
 void setup()
 {
   Serial.begin(115200);
-  Serial.println("Begin SX1280 testing...");
-  pinMode(PC13, OUTPUT);
+  // 发送和接收使能位
   pinMode(GPIO_PIN_TX_EN, OUTPUT);
   pinMode(GPIO_PIN_RX_EN, OUTPUT);
-
+  // LED
+  pinMode(PC13, OUTPUT);
+  // 初始化跳频随机序列
   FHSSrandomiseFHSSsequence(uidMacSeedGet());
+
   Radio.TXdoneCallback = &TXdoneCallback;
   Radio.RXdoneCallback = &RXdoneCallback;
 
-  Radio.currFreq = FHSSgetInitialFreq(); //set frequency first or an error will occur!!!
+  Radio.currFreq = FHSSgetInitialFreq(); 
 
+  // RF初始化
   bool init_success;
   init_success = Radio.Begin(FHSSgetMinimumFreq(), FHSSgetMaximumFreq());
   if(!init_success){Serial.println("Radio.Begin failed!");}
-
   SetRFLinkRate(9);
-
+  // 开启连续接收模式
   Radio.RXnb(SX1280_MODE_RX_CONT);
 }
 
-// bool busy;
+
 void loop()
 {
-  Serial.println("Current Index = " + String(FHSSgetCurrIndex()));
-  Serial.println(Radio.GetRssiInst(SX12XX_Radio_1));
-  Radio.GetStatus(SX12XX_Radio_1);
-  
-  // busy = digitalRead(GPIO_PIN_BUSY);
 
-  delay(500);
-  yield();
 }
--- src/tx_main.cpp
+++ src/tx_main.cpp
@@ -5,21 +5,23 @@
 #include "FHSS.h"
 
 uint8_t testdata[] = "HELLO!";
-
-// every X packets we hop to a new frequency. 
-// Max value of 16 since only 4 bits have been assigned in the sync package.
 uint8_t FHSShopInterval = 4;    
+uint8_t IntervalCount = 0;
 
-uint8_t  IntervalCount = 0;
 
 void ICACHE_RAM_ATTR TXdoneCallback()
 {
-  // IntervalCount++;
-  // if(IntervalCount % 4 == 0){Serial.println(FHSSgetNextFreq());}
-
+  IntervalCount++;
+  if(IntervalCount % 4 == 0)
+  {
+    Serial.println(FHSSgetNextFreq());
+  }
+  // 发送完数据翻转LED
   digitalToggle(PC13);
+  // 打印状态信息
+  Serial.println("RSSI = " + Radio.GetRssiInst(SX12XX_Radio_1));
+  Radio.GetStatus(SX12XX_Radio_1);
 
-  Serial.println("Current Index = " + String(FHSSgetCurrIndex()));
 }
 
 bool ICACHE_RAM_ATTR RXdoneCallback(SX12xxDriverCommon::rx_status const /*status*/)
@@ -27,65 +29,48 @@ bool ICACHE_RAM_ATTR RXdoneCallback(SX12xxDriverCommon::rx_status const /*status
   return true;
 }
 
+// 初始化空中速率
 void SetRFLinkRate(uint8_t index)
 {
   expresslrs_mod_settings_s *const ModParams = get_elrs_airRateConfig(index);
-  // expresslrs_rf_pref_params_s *const RFperf = get_elrs_RFperfParams(index);
-
-  bool invertIQ = InBindingMode || (UID[5] & 0x01);
-
-  // FHSSusePrimaryFreqBand = !(ModParams->radio_type == RADIO_TYPE_LR1121_LORA_2G4) && 
-  //                           !(ModParams->radio_type == RADIO_TYPE_LR1121_GFSK_2G4);
-  // // FHSSuseDualBand = ModParams->radio_type == RADIO_TYPE_LR1121_LORA_DUAL;
 
   Radio.Config(ModParams->bw, ModParams->sf, ModParams->cr, FHSSgetInitialFreq(), 
-              ModParams->PreambleLen, invertIQ, ModParams->PayloadLength, ModParams->interval, 
+              ModParams->PreambleLen, false, ModParams->PayloadLength, ModParams->interval, 
               uidMacSeedGet(), 0, (ModParams->radio_type == RADIO_TYPE_SX128x_FLRC));
 
-
-  // InitialFreq has been set, so lets also reset the FHSS Idx and Nonce.
-  // FHSSsetCurrIndex(0);
-
   ExpressLRS_currAirRate_Modparams = ModParams;
-  // ExpressLRS_currAirRate_RFperfParams = RFperf;
-
 }
 
 void setup()
 {
   Serial.begin(115200);
-  pinMode(PC13, OUTPUT);
+  // 发送和接收使能位
   pinMode(GPIO_PIN_TX_EN, OUTPUT);
   pinMode(GPIO_PIN_RX_EN, OUTPUT);
-
-
+  // LED
+  pinMode(PC13, OUTPUT);
+  // 初始化跳频随机序列
   FHSSrandomiseFHSSsequence(uidMacSeedGet());
+
   Radio.TXdoneCallback = &TXdoneCallback;
   Radio.RXdoneCallback = &RXdoneCallback;
 
   Radio.currFreq = FHSSgetInitialFreq(); 
 
+  // RF初始化
   bool init_success;
   init_success = Radio.Begin(FHSSgetMinimumFreq(), FHSSgetMaximumFreq());
   if(!init_success){Serial.println("Radio.Begin failed!");}
-
   SetRFLinkRate(9);
 
-  // Radio.TXnb(testdata, sizeof(testdata), SX12XX_Radio_1);
 }
-
-bool busy;
-
 
 void loop()
 {
   Radio.TXnb(testdata, sizeof(testdata), SX12XX_Radio_1);
 
-  busy = digitalRead(GPIO_PIN_BUSY);
-  Serial.println(Radio.GetRssiInst(SX12XX_Radio_1));
-  Radio.GetStatus(SX12XX_Radio_1);
-
   delay(1000);
+
   yield();
 }
 
