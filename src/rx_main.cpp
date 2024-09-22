2d4ddc24f876b2c93ac78380b7c94edc2a4fbcdc
xjy <2444717591@qq.com>
2024-09-22 16:43:55 +0800

    正常收发 似乎有在跳频

--- src/rx_main.cpp
+++ src/rx_main.cpp
@@ -4,48 +4,55 @@
 #include "common.h"
 #include "FHSS.h"
 
-// uint8_t FHSShopInterval = 4;    
-// uint8_t IntervalCount = 0;
+uint8_t FHSShopInterval = 4;    
+uint8_t IntervalCount = 0;
+uint8_t ACK = 0xAA;
+uint8_t packetSize = 6;
 
+// TX 中断回调函数
 void ICACHE_RAM_ATTR TXdoneCallback()
 {
-  // IntervalCount++;
-  // if(IntervalCount % FHSShopInterval == 0)
-  // {
-  //   Serial.println(FHSSgetNextFreq());
-  // }
+  // 切换回接收模式
+  Radio.RXnb(SX1280_MODE_RX_CONT);
+  // 计算跳频
+  IntervalCount++;
+  if(IntervalCount % FHSShopInterval == 0)
+  {
+    Serial.println(FHSSgetNextFreq());
+  }
 }
-
-bool ICACHE_RAM_ATTR RXdoneCallback(SX12xxDriverCommon::rx_status const /*status*/)
+// RX 中断回调函数
+bool ICACHE_RAM_ATTR RXdoneCallback(SX12xxDriverCommon::rx_status const status)
 {
   // 如果接收到数据，LED 灯会和发送方同时闪烁。
   digitalToggle(PC13);
   // 打印 RXdataBuffer
-  for (int i = 0; i < 16; i++)
-  {
-      Serial.print((char)Radio.RXdataBuffer[i]);
+  for (int i = 0; i < packetSize; i++){
+    Serial.print((char)Radio.RXdataBuffer[i]);
   }
   Serial.println("");
+  // 打印状态信息
+  // Radio.GetStatus(SX12XX_Radio_1);
+  // Serial.println(Radio.GetRssiInst(SX12XX_Radio_1));
+  // Serial.println("Initial Freq = " + String(FHSSgetInitialFreq()));
+  // Serial.println("Sequense Count = " + String(FHSSgetSequenceCount()));
+  // Serial.println("Channel Count = " + String(FHSSgetChannelCount()));
+  Serial.println("Current Index = " + String(FHSSgetCurrIndex()));
 
   // 发送应答包
-  // Radio.TXnb()
+  Radio.TXnb(&ACK, 1, SX12XX_Radio_1);
   return true;
 }
-
 // 初始化空中速率
-void SetRFLinkRate(uint8_t index)
-{
+void SetRFLinkRate(uint8_t index){
   expresslrs_mod_settings_s *const ModParams = get_elrs_airRateConfig(index);
-
   Radio.Config(ModParams->bw, ModParams->sf, ModParams->cr, FHSSgetInitialFreq(), 
               ModParams->PreambleLen, false, ModParams->PayloadLength, ModParams->interval, 
               uidMacSeedGet(), 0, (ModParams->radio_type == RADIO_TYPE_SX128x_FLRC));
-
   ExpressLRS_currAirRate_Modparams = ModParams;
 }
 
-void setup()
-{
+void setup(){
   Serial.begin(115200);
   // 发送和接收使能位
   pinMode(GPIO_PIN_TX_EN, OUTPUT);
@@ -54,12 +61,9 @@ void setup()
   pinMode(PC13, OUTPUT);
   // 初始化跳频随机序列
   FHSSrandomiseFHSSsequence(uidMacSeedGet());
-
   Radio.TXdoneCallback = &TXdoneCallback;
   Radio.RXdoneCallback = &RXdoneCallback;
-
   Radio.currFreq = FHSSgetInitialFreq(); 
-
   // RF初始化
   bool init_success;
   init_success = Radio.Begin(FHSSgetMinimumFreq(), FHSSgetMaximumFreq());
@@ -70,16 +74,13 @@ void setup()
 }
 
 
-void loop()
-{
+void loop(){
   // 打印状态信息
-  Radio.GetStatus(SX12XX_Radio_1);
-  Serial.println(Radio.GetRssiInst(SX12XX_Radio_1));
+  // Radio.GetStatus(SX12XX_Radio_1);
+  // Serial.println(Radio.GetRssiInst(SX12XX_Radio_1));
   // Serial.println("Initial Freq = " + String(FHSSgetInitialFreq()));
   // Serial.println("Sequense Count = " + String(FHSSgetSequenceCount()));
-  // Serial.println("Current Index = " + String(FHSSgetCurrIndex()));
   // Serial.println("Channel Count = " + String(FHSSgetChannelCount()));
 
-  delay(500);
   yield();
 }
--- src/tx_main.cpp
+++ src/tx_main.cpp
@@ -4,41 +4,44 @@
 #include "common.h"
 #include "FHSS.h"
 
-uint8_t testdata[] = "HELLO!";
-// uint8_t FHSShopInterval = 4;    
-// uint8_t IntervalCount = 0;
+uint8_t data[] = "HELLO!";
+
+uint8_t FHSShopInterval = 4;    
+uint8_t IntervalCount = 0;
+uint8_t ACK = 0xAA;
+// bool waitingACK = false;
+// uint8_t missedPacket = 0;
 
 
-void ICACHE_RAM_ATTR TXdoneCallback()
-{
-  // IntervalCount++;
-  // if(IntervalCount % 4 == 0)
-  // {
-  //   Serial.println(FHSSgetNextFreq());
-  // }
+// TX 中断回调函数
+void ICACHE_RAM_ATTR TXdoneCallback(){
   // 发送完数据翻转LED
   digitalToggle(PC13);
+  // 等待ACK
+  Radio.RXnb(SX1280_MODE_RX_CONT);
+  // waitingACK = true;
 }
-
-bool ICACHE_RAM_ATTR RXdoneCallback(SX12xxDriverCommon::rx_status const /*status*/)
-{
+// RX 中断回调函数
+bool ICACHE_RAM_ATTR RXdoneCallback(SX12xxDriverCommon::rx_status const status){
+  if(Radio.RXdataBuffer[1] == ACK){
+    IntervalCount++;
+    if(IntervalCount % 4 == 0){
+      Serial.println(FHSSgetNextFreq());
+    }
+  }
   return true;
 }
-
 // 初始化空中速率
-void SetRFLinkRate(uint8_t index)
-{
+void SetRFLinkRate(uint8_t index){
+  // 获取参数、配置参数
   expresslrs_mod_settings_s *const ModParams = get_elrs_airRateConfig(index);
-
   Radio.Config(ModParams->bw, ModParams->sf, ModParams->cr, FHSSgetInitialFreq(), 
               ModParams->PreambleLen, false, ModParams->PayloadLength, ModParams->interval, 
               uidMacSeedGet(), 0, (ModParams->radio_type == RADIO_TYPE_SX128x_FLRC));
-
   ExpressLRS_currAirRate_Modparams = ModParams;
 }
 
-void setup()
-{
+void setup(){
   Serial.begin(115200);
   // 发送和接收使能位
   pinMode(GPIO_PIN_TX_EN, OUTPUT);
@@ -47,12 +50,9 @@ void setup()
   pinMode(PC13, OUTPUT);
   // 初始化跳频随机序列
   FHSSrandomiseFHSSsequence(uidMacSeedGet());
-
   Radio.TXdoneCallback = &TXdoneCallback;
   Radio.RXdoneCallback = &RXdoneCallback;
-
   Radio.currFreq = FHSSgetInitialFreq(); 
-
   // RF初始化
   bool init_success;
   init_success = Radio.Begin(FHSSgetMinimumFreq(), FHSSgetMaximumFreq());
@@ -60,21 +60,24 @@ void setup()
   SetRFLinkRate(9);
 }
 
-void loop()
-{
-  Radio.TXnb(testdata, sizeof(testdata), SX12XX_Radio_1);
-
-
+void loop(){
+  // if(waitingACK){
+  //   missedPacket++;
+  //   if(!(missedPacket %= 3)){
+  //     Radio.SetFrequencyReg(FHSSgetInitialFreq());
+  //     Radio.currFreq = FHSSgetInitialFreq(); 
+  //   }
+  //   waitingACK = false;
+  // }
+  Radio.TXnb(data, sizeof(data), SX12XX_Radio_1);
   // 打印状态信息
-  Radio.GetStatus(SX12XX_Radio_1);
-  // 发送模式时RSSI恒为-127
-  Serial.println(Radio.GetRssiInst(SX12XX_Radio_1));
+  // Radio.GetStatus(SX12XX_Radio_1);
+  // Serial.println(Radio.GetRssiInst(SX12XX_Radio_1));
   // Serial.println("Initial Freq = " + String(FHSSgetInitialFreq()));
   // Serial.println("Sequense Count = " + String(FHSSgetSequenceCount()));
-  // Serial.println("Current Index = " + String(FHSSgetCurrIndex()));
   // Serial.println("Channel Count = " + String(FHSSgetChannelCount()));
-
-
+  // Serial.println("Current Index = " + String(FHSSgetCurrIndex()));
+  // Serial.println(missedPacket);
   delay(1000);
   yield();
 }
