2e1b118ae1a8a635ff75e948dcb9bbca31284a0e
xjy <2444717591@qq.com>
2024-10-08 10:51:28 +0800

    txrx sending and receiving UID

--- src/rx_main.cpp
+++ src/rx_main.cpp
@@ -1,67 +1,25 @@
 #include <Arduino.h>
+
 #include "targets.h"
-#include "SX1280Driver.h"
 #include "common.h"
+
+#include "SX1280Driver.h"
 #include "FHSS.h"
 
 #include <Adafruit_SSD1306.h>
-#define OLED_RESET     4 // 定义 OLED 的复位引脚
-#define SCREEN_WIDTH   128 // OLED 屏幕宽度
-#define SCREEN_HEIGHT  64  // OLED 屏幕高度
-// 初始化 OLED 显示器
+
+#define OLED_RESET     4 
+#define SCREEN_WIDTH   128 
+#define SCREEN_HEIGHT  64
+
 Adafruit_SSD1306 display(OLED_RESET);
 
-uint8_t FHSShopInterval = 4;    
-uint8_t IntervalCount = 0;
-uint32_t currentFreq;
-uint8_t ACK = 0x00;
-uint8_t packetSize = 6;
+bool inBindingMode;
+#define PacketType_BIND   0
+#define PacketType_DATA   1  
+// Bound is any of the last 4 bytes nonzero (unbound is all zeroes)
+#define UID_IS_BOUND(uid) (uid[2] != 0 || uid[3] != 0 || uid[4] != 0 || uid[5] != 0)
 
-// TX ACK中断回调函数
-void ICACHE_RAM_ATTR TXdoneCallback()
-{
-  // 计算跳频
-  ACK++;
-  IntervalCount++;
-  if(IntervalCount % FHSShopInterval == 0)
-  {
-    IntervalCount = 0;
-    currentFreq = FHSSgetNextFreq();
-    Radio.SetFrequencyReg(currentFreq);
-  }
-  // 切换回接收模式
-  Radio.RXnb(SX1280_MODE_RX_CONT);
-}
-// RX 中断回调函数
-bool ICACHE_RAM_ATTR RXdoneCallback(SX12xxDriverCommon::rx_status const status)
-{
-  // 如果接收到数据，LED 灯会和发送方同时闪烁。
-  digitalToggle(PC13);
-  // 打印 RXdataBuffer
-  for (int i = 0; i < packetSize; i++){
-    Serial.print((char)Radio.RXdataBuffer[i]);
-  }
-  Serial.println("");
-  // 打印状态信息
-  // Radio.GetStatus(SX12XX_Radio_1);
-  // Serial.println(Radio.GetRssiInst(SX12XX_Radio_1));
-  // Serial.println("Initial Freq = " + String(FHSSgetInitialFreq()));
-  // Serial.println("Sequense Count = " + String(FHSSgetSequenceCount()));
-  // Serial.println("Channel Count = " + String(FHSSgetChannelCount()));
-  // Serial.println("Current Index = " + String(FHSSgetCurrIndex()));
-    Serial.println(currentFreq);
-
-    display.clearDisplay();             // 清除屏幕
-    display.setCursor(0, 0);            // 设置光标位置
-    display.println(Radio.GetRssiInst(SX12XX_Radio_1));    // 显示文本
-    display.setCursor(12, 16);            // 设置光标位置
-    display.println(currentFreq);       // 显示文本
-    display.display();
-  // 发送应答包
-  Radio.TXnb(&ACK, 1, SX12XX_Radio_1);
-  return true;
-}
-// 初始化空中速率
 void SetRFLinkRate(uint8_t index){
   expresslrs_mod_settings_s *const ModParams = get_elrs_airRateConfig(index);
   Radio.Config(ModParams->bw, ModParams->sf, ModParams->cr, FHSSgetInitialFreq(), 
@@ -70,50 +28,109 @@ void SetRFLinkRate(uint8_t index){
   ExpressLRS_currAirRate_Modparams = ModParams;
 }
 
+bool ICACHE_RAM_ATTR RXdoneCallback(SX12xxDriverCommon::rx_status const status)
+{
+  if(inBindingMode && Radio.RXdataBuffer[0] == PacketType_BIND)
+  {
+    uint8_t payloadsize = Radio.RXdataBuffer[1];
+    for(int i = 0; i < payloadsize ; i++)
+    {
+      UID[i+2] = Radio.RXdataBuffer[i+2];
+    }
+    inBindingMode = false;
+
+    // 打印 RXdataBuffer
+    for (int i = 0; i < 16; i++)
+    {
+      Serial.print(Radio.RXdataBuffer[i]);
+      Serial.print(" ");
+    }
+    Serial.println(" ");
+
+    Serial.print("UID = ");
+    for(int i = 0; i < 6; i++)  
+    {
+      Serial.print(UID[i]);
+      Serial.print(" ");
+    }
+    Serial.println(" ");  
+    // SetRFLinkRate(enumRatetoIndex(RATE_LORA_500HZ));
+  // }
+  }
+    return true;
+}
+
 void setup()
 {
-  // 初始化 I2C 总线
+  // UART
+  Serial.begin(115200);
+  // LED
+  pinMode(PC13, OUTPUT);
+  // OLED
   Wire.setSCL(PB8);
   Wire.setSDA(PB9);
   Wire.begin();
-    // 初始化 OLED 显示器
-  if (!display.begin(SSD1306_SWITCHCAPVCC, 0x3C)) { // OLED 地址通常为 0x3C 或 0x3D
+  if (!display.begin(SSD1306_SWITCHCAPVCC, 0x3C)) { 
     Serial.println(F("SSD1306 allocation failed"));
-    for(;;); // 不成功则停止
+    for(;;); 
   }
-  display.setTextSize(2);             // 正常文本大小
-  display.setTextColor(WHITE);        // 文本颜色为白色
-  display.clearDisplay(); // 清除屏幕
+  display.setTextSize(1);            
+  display.setTextColor(WHITE);        
+  display.clearDisplay(); 
   display.display();
-
-  Serial.begin(115200);
-  // 发送和接收使能位
+  // SX1280
   pinMode(GPIO_PIN_TX_EN, OUTPUT);
   pinMode(GPIO_PIN_RX_EN, OUTPUT);
-  // LED
-  pinMode(PC13, OUTPUT);
-  // 初始化跳频随机序列
   FHSSrandomiseFHSSsequence(uidMacSeedGet());
-  Radio.TXdoneCallback = &TXdoneCallback;
   Radio.RXdoneCallback = &RXdoneCallback;
   Radio.currFreq = FHSSgetInitialFreq(); 
-  // RF初始化
-  bool init_success;
-  init_success = Radio.Begin(FHSSgetMinimumFreq(), FHSSgetMaximumFreq());
-  if(!init_success){Serial.println("Radio.Begin failed!");}
-  SetRFLinkRate(9);
+  Radio.Begin(FHSSgetMinimumFreq(), FHSSgetMaximumFreq());
+  SetRFLinkRate(enumRatetoIndex(RATE_LORA_500HZ));
   // 开启连续接收模式
   Radio.RXnb(SX1280_MODE_RX_CONT);
 }
 
+void enterBindingMode()
+{
+  SetRFLinkRate(enumRatetoIndex(RATE_BINDING));
+  Radio.RXnb(SX1280_MODE_RX_CONT);
+}
 
-void loop(){
-  // 打印状态信息
-  // Radio.GetStatus(SX12XX_Radio_1);
-  // Serial.println(Radio.GetRssiInst(SX12XX_Radio_1));
-  // Serial.println("Initial Freq = " + String(FHSSgetInitialFreq()));
-  // Serial.println("Sequense Count = " + String(FHSSgetSequenceCount()));
-  // Serial.println("Channel Count = " + String(FHSSgetChannelCount()));
+void loop()
+{
+  if(inBindingMode == false && !UID_IS_BOUND(UID))
+  {
+    inBindingMode = true;
+    enterBindingMode();
+    display.clearDisplay();             
+    display.setCursor(0, 0);            
+    display.println("receiving UID...");
+    display.display();
+  }
+  display.clearDisplay();            
+  display.setCursor(0, 0);           
+  display.println("UID");
+        
+  display.setCursor(0, 8);            
+  display.println(UID[0]);
 
+  display.setCursor(18, 8);            
+  display.println(UID[1]);
+
+  display.setCursor(40, 8);            
+  display.println(UID[2]);
+
+  display.setCursor(56, 8);            
+  display.println(UID[3]);
+
+  display.setCursor(72, 8);            
+  display.println(UID[4]);
+
+  display.setCursor(88, 8);            
+  display.println(UID[5]);
+  display.display();
+
+  digitalToggle(PC13);
+  delay(200);
   yield();
 }
--- src/tx_main.cpp
+++ src/tx_main.cpp
@@ -1,46 +1,57 @@
 #include <Arduino.h>
+#include <time.h>
+
 #include "targets.h"
-#include "SX1280Driver.h"
 #include "common.h"
+
+#include "SX1280Driver.h"
 #include "FHSS.h"
 
-uint8_t data[] = "HELLO!";
+#include <Adafruit_SSD1306.h>
 
-uint8_t FHSShopInterval = 4;    
-uint8_t IntervalCount = 0;
-uint8_t ACK = 0x00;
-// bool waitingACK = false;
-// uint8_t missedPacket = 0;
+#define OLED_RESET     4 
+#define SCREEN_WIDTH   128 
+#define SCREEN_HEIGHT  64
 
+Adafruit_SSD1306 display(OLED_RESET);
 
-// TX 中断回调函数
+#define PacketType_BIND   0
+#define PacketType_DATA   1  
+
+typedef struct __attribute__((packed)) {
+    uint8_t   type;
+    // uint8_t   IntervalCount;
+    // uint8_t   currentchannel;
+    uint8_t   payloadSize;
+    uint8_t   payload[6];
+} Packet_t;
+
+Packet_t packet;
+
+#define BindingSpamAmount 25
+bool inBindingMode;
+static uint8_t BindingSendCount;
+uint32_t BindingTime;
+uint8_t sendUIDcount;
+
+// uint8_t FHSShopInterval = 4;    
+// uint8_t IntervalCount = 0;
+// uint32_t currentFreq = 0;
+
 void ICACHE_RAM_ATTR TXdoneCallback()
 {
-  // 发送完数据翻转LED
+  // IntervalCount++;
+  // if(IntervalCount % FHSShopInterval == 0)
+  // {
+  //   IntervalCount = 0;
+  //   currentFreq = FHSSgetNextFreq();
+  //   Radio.SetFrequencyReg(currentFreq);
+  // }
   digitalToggle(PC13);
-  // 等待ACK
-  Radio.RXnb(SX1280_MODE_RX_CONT);
-  // waitingACK = true;
 }
-// RX 中断回调函数
-bool ICACHE_RAM_ATTR RXdoneCallback(SX12xxDriverCommon::rx_status const status)
+
+void SetRFLinkRate(uint8_t index)
 {
-  if(Radio.RXdataBuffer[0] == ACK){
-    ACK++;
-    IntervalCount++;
-    if(IntervalCount % 4 == 0)
-    {
-      IntervalCount = 0;
-      uint32_t currentFreq =FHSSgetNextFreq();
-      Radio.SetFrequencyReg(currentFreq);
-      Serial.println(currentFreq);
-    }
-  }
-  return true;
-}
-// 初始化空中速率
-void SetRFLinkRate(uint8_t index){
-  // 获取参数、配置参数
   expresslrs_mod_settings_s *const ModParams = get_elrs_airRateConfig(index);
   Radio.Config(ModParams->bw, ModParams->sf, ModParams->cr, FHSSgetInitialFreq(), 
               ModParams->PreambleLen, false, ModParams->PayloadLength, ModParams->interval, 
@@ -48,45 +59,124 @@ void SetRFLinkRate(uint8_t index){
   ExpressLRS_currAirRate_Modparams = ModParams;
 }
 
-void setup(){
-  Serial.begin(115200);
-  // 发送和接收使能位
-  pinMode(GPIO_PIN_TX_EN, OUTPUT);
-  pinMode(GPIO_PIN_RX_EN, OUTPUT);
-  // LED
-  pinMode(PC13, OUTPUT);
-  // 初始化跳频随机序列
-  FHSSrandomiseFHSSsequence(uidMacSeedGet());
-  Radio.TXdoneCallback = &TXdoneCallback;
-  Radio.RXdoneCallback = &RXdoneCallback;
-  Radio.currFreq = FHSSgetInitialFreq(); 
-  // RF初始化
-  bool init_success;
-  init_success = Radio.Begin(FHSSgetMinimumFreq(), FHSSgetMaximumFreq());
-  if(!init_success){Serial.println("Radio.Begin failed!");}
-  SetRFLinkRate(9);
+static void setupBindingFromConfig()
+{
+  UID[0] = (uint8_t)HAL_GetUIDw0();
+  UID[1] = (uint8_t)(HAL_GetUIDw0() >> 8);
+  UID[2] = (uint8_t)HAL_GetUIDw1();
+  UID[3] = (uint8_t)(HAL_GetUIDw1() >> 8);
+  UID[4] = (uint8_t)HAL_GetUIDw2();
+  UID[5] = (uint8_t)(HAL_GetUIDw2() >> 8);
+
+  Serial.print("UID = ");
+  for(int i = 0; i < 6; i++)  
+  {
+    Serial.print(UID[i]);
+    Serial.print(" ");
+  }
+  Serial.println(" ");
 }
 
-void loop(){
-  // if(waitingACK){
-  //   missedPacket++;
-  //   if(!(missedPacket %= 3)){
-  //     Radio.SetFrequencyReg(FHSSgetInitialFreq());
-  //     Radio.currFreq = FHSSgetInitialFreq(); 
-  //   }
-  //   waitingACK = false;
-  // }
-  Radio.TXnb(data, sizeof(data), SX12XX_Radio_1);
-  // 打印状态信息
-  // Radio.GetStatus(SX12XX_Radio_1);
-  // Serial.println(Radio.GetRssiInst(SX12XX_Radio_1));
-  // Serial.println("Initial Freq = " + String(FHSSgetInitialFreq()));
-  // Serial.println("Sequense Count = " + String(FHSSgetSequenceCount()));
-  // Serial.println("Channel Count = " + String(FHSSgetChannelCount()));
-  Serial.println("Current Index = " + String(FHSSgetCurrIndex()));
-  // Serial.println(missedPacket);
-  delay(1000);
-  yield();
+void handleButtonPress() 
+{
+  if(!inBindingMode)
+  {
+    inBindingMode = true;
+    BindingTime = millis();
+  }   
+  digitalToggle(PC13);
+}
+
+void setup()
+{
+  // UART
+  Serial.begin(115200);
+  // LED
+  pinMode(PC13, OUTPUT);
+  // Button
+  pinMode(PB1, INPUT_PULLUP);            
+  attachInterrupt(digitalPinToInterrupt(PB1), handleButtonPress, FALLING);  
+  // OLED
+  Wire.setSCL(PB8);
+  Wire.setSDA(PB9);
+  Wire.begin();
+  if (!display.begin(SSD1306_SWITCHCAPVCC, 0x3C)) { 
+    Serial.println(F("SSD1306 allocation failed"));
+    for(;;); 
+  }
+  display.setTextSize(1);            
+  display.setTextColor(WHITE);        
+  display.clearDisplay(); 
+  display.display();
+  // SX1280
+  setupBindingFromConfig();
+  pinMode(GPIO_PIN_TX_EN, OUTPUT);
+  pinMode(GPIO_PIN_RX_EN, OUTPUT);
+  FHSSrandomiseFHSSsequence(uidMacSeedGet());
+  Radio.TXdoneCallback = &TXdoneCallback;
+  Radio.currFreq = FHSSgetInitialFreq(); 
+  Radio.Begin(FHSSgetMinimumFreq(), FHSSgetMaximumFreq());
+  SetRFLinkRate(enumRatetoIndex(RATE_LORA_500HZ));
+}
+
+void loop()
+{
+  if(inBindingMode)
+  {
+    SetRFLinkRate(enumRatetoIndex(RATE_BINDING));
+    sendUIDcount = 0;
+    packet.type = PacketType_BIND;
+    packet.payloadSize = 4;
+    
+    for(int i = 0; i < packet.payloadSize; i++)
+    {
+      packet.payload[i] = UID[i+2];
+    }
+    while(millis() - BindingTime < 5000)
+    {
+      Radio.TXnb((uint8_t*)&packet, sizeof(packet), SX12XX_Radio_1);
+      sendUIDcount++;
+      delay(500);
+      display.clearDisplay();             
+      display.setCursor(0, 0);            
+      display.println("sending UID...");
+      display.setCursor(0, 8);            
+      display.println(sendUIDcount);
+      display.display();
+    }
+    inBindingMode = false;  
+    SetRFLinkRate(enumRatetoIndex(RATE_LORA_500HZ));
+  }
+  else
+  {
+    // Radio.TXnb(data, sizeof(data), SX12XX_Radio_1);
+
+    // 调试信息
+    // Radio.GetStatus(SX12XX_Radio_1);
+    // Serial.println(Radio.GetRssiInst(SX12XX_Radio_1));
+    // Serial.println("Initial Freq = " + String(FHSSgetInitialFreq()));
+    // Serial.println("Sequense Count = " + String(FHSSgetSequenceCount()));
+    // Serial.println("Channel Count = " + String(FHSSgetChannelCount()));
+    // Serial.println("Current Index = " + String(FHSSgetCurrIndex()));
+    display.clearDisplay();            
+    display.setCursor(0, 0);           
+    display.println("UID");         
+    display.setCursor(0, 8);            
+    display.println(packet.payload[0]);
+    display.setCursor(18, 8);            
+    display.println(packet.payload[1]);
+    display.setCursor(40, 8);            
+    display.println(packet.payload[2]);
+    display.setCursor(56, 8);            
+    display.println(packet.payload[3]);
+    display.setCursor(72, 8);            
+    display.println(packet.payload[4]);
+    display.setCursor(88, 8);            
+    display.println(packet.payload[5]);
+    display.display();
+    delay(1000);
+    yield();
+  }
 }
 
 
