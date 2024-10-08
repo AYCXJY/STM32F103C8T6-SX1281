ace37e31042e40e15bb5f6c3f4aac4fa1c99149e
xjy <2444717591@qq.com>
2024-10-08 13:48:02 +0800

    RX add eeprom and button

--- src/rx_main.cpp
+++ src/rx_main.cpp
@@ -1,7 +1,10 @@
 #include <Arduino.h>
+#include <time.h>
 
 #include "targets.h"
 #include "common.h"
+
+#include "elrs_eeprom.h"
 
 #include "SX1280Driver.h"
 #include "FHSS.h"
@@ -13,14 +16,16 @@
 #define SCREEN_HEIGHT  64
 
 Adafruit_SSD1306 display(OLED_RESET);
+ELRS_EEPROM eeprom;
 
-bool inBindingMode;
 #define PacketType_BIND   0
 #define PacketType_DATA   1  
-// Bound is any of the last 4 bytes nonzero (unbound is all zeroes)
-#define UID_IS_BOUND(uid) (uid[2] != 0 || uid[3] != 0 || uid[4] != 0 || uid[5] != 0)
 
-void SetRFLinkRate(uint8_t index){
+bool inBindingMode;
+#define UID_IS_BOUND(uid) (uid[2] != 255 || uid[3] != 255 || uid[4] != 255 || uid[5] != 255)
+
+void SetRFLinkRate(uint8_t index)
+{
   expresslrs_mod_settings_s *const ModParams = get_elrs_airRateConfig(index);
   Radio.Config(ModParams->bw, ModParams->sf, ModParams->cr, FHSSgetInitialFreq(), 
               ModParams->PreambleLen, false, ModParams->PayloadLength, ModParams->interval, 
@@ -28,34 +33,53 @@ void SetRFLinkRate(uint8_t index){
   ExpressLRS_currAirRate_Modparams = ModParams;
 }
 
+void enterbindingmode(void)
+{
+  SetRFLinkRate(enumRatetoIndex(RATE_BINDING));
+  Radio.RXnb(SX1280_MODE_RX_CONT);
+  inBindingMode = true;
+}
+
+void exitbindingmode(void)
+{
+  SetRFLinkRate(enumRatetoIndex(RATE_LORA_500HZ));
+  Radio.RXnb(SX1280_MODE_RX_CONT);
+  inBindingMode = false;
+}
+
+void handleButtonPress() 
+{
+  enterbindingmode();
+}
+
 bool ICACHE_RAM_ATTR RXdoneCallback(SX12xxDriverCommon::rx_status const status)
 {
   if(inBindingMode && Radio.RXdataBuffer[0] == PacketType_BIND)
   {
+    // 
     uint8_t payloadsize = Radio.RXdataBuffer[1];
-    for(int i = 0; i < payloadsize ; i++)
-    {
-      UID[i+2] = Radio.RXdataBuffer[i+2];
+    for(int i = 0; i < payloadsize ; i++){
+      UID[i] = Radio.RXdataBuffer[i+2];
     }
-    inBindingMode = false;
+    // put UID to eeprom
+    eeprom.Put(0, UID);
+    eeprom.Commit();
+    exitbindingmode();
 
-    // 打印 RXdataBuffer
-    for (int i = 0; i < 16; i++)
-    {
-      Serial.print(Radio.RXdataBuffer[i]);
-      Serial.print(" ");
-    }
-    Serial.println(" ");
-
-    Serial.print("UID = ");
-    for(int i = 0; i < 6; i++)  
-    {
-      Serial.print(UID[i]);
-      Serial.print(" ");
-    }
-    Serial.println(" ");  
-    // SetRFLinkRate(enumRatetoIndex(RATE_LORA_500HZ));
-  // }
+    // // 打印 RXdataBuffer
+    // for (int i = 0; i < 16; i++)
+    // {
+    //   Serial.print(Radio.RXdataBuffer[i]);
+    //   Serial.print(" ");
+    // }
+    // Serial.println(" ");
+    // Serial.print("UID = ");
+    // for(int i = 0; i < 6; i++)  
+    // {
+    //   Serial.print(UID[i]);
+    //   Serial.print(" ");
+    // }
+    // Serial.println(" ");  
   }
     return true;
 }
@@ -66,6 +90,9 @@ void setup()
   Serial.begin(115200);
   // LED
   pinMode(PC13, OUTPUT);
+  // Button
+  pinMode(PB1, INPUT_PULLUP);            
+  attachInterrupt(digitalPinToInterrupt(PB1), handleButtonPress, FALLING);  
   // OLED
   Wire.setSCL(PB8);
   Wire.setSDA(PB9);
@@ -88,49 +115,47 @@ void setup()
   SetRFLinkRate(enumRatetoIndex(RATE_LORA_500HZ));
   // 开启连续接收模式
   Radio.RXnb(SX1280_MODE_RX_CONT);
-}
-
-void enterBindingMode()
-{
-  SetRFLinkRate(enumRatetoIndex(RATE_BINDING));
-  Radio.RXnb(SX1280_MODE_RX_CONT);
+  // eeprom
+  eeprom.Begin();
+  eeprom.Get(0, UID);
+  if(!UID_IS_BOUND(UID))
+  {
+    enterbindingmode();
+  }
 }
 
 void loop()
 {
-  if(inBindingMode == false && !UID_IS_BOUND(UID))
+  if(inBindingMode)
   {
-    inBindingMode = true;
-    enterBindingMode();
     display.clearDisplay();             
     display.setCursor(0, 0);            
     display.println("receiving UID...");
     display.display();
   }
-  display.clearDisplay();            
-  display.setCursor(0, 0);           
-  display.println("UID");
-        
-  display.setCursor(0, 8);            
-  display.println(UID[0]);
+  else
+  {
+    display.clearDisplay();    
+    // print UID
+    display.setCursor(0, 0);           
+    display.println("UID");         
+    display.setCursor(0, 8);            
+    display.println(UID[0]);
+    display.setCursor(24, 8);            
+    display.println(UID[1]);
+    display.setCursor(48, 8);            
+    display.println(UID[2]);
+    display.setCursor(0, 16);            
+    display.println(UID[3]);
+    display.setCursor(24, 16);            
+    display.println(UID[4]);
+    display.setCursor(48, 16);            
+    display.println(UID[5]);
+    // print RXBuff
+    display.setCursor(0, 24);            
+    display.println(&Radio.RXdataBuffer);
 
-  display.setCursor(18, 8);            
-  display.println(UID[1]);
-
-  display.setCursor(40, 8);            
-  display.println(UID[2]);
-
-  display.setCursor(56, 8);            
-  display.println(UID[3]);
-
-  display.setCursor(72, 8);            
-  display.println(UID[4]);
-
-  display.setCursor(88, 8);            
-  display.println(UID[5]);
-  display.display();
-
-  digitalToggle(PC13);
-  delay(200);
+    display.display();
+  }
   yield();
 }
